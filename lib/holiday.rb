5351912fc959acbf1bba82989ce7a669a4eb16bd
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-09 14:54:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,9 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season,holidays|
-    puts season.to_s.capitalize+":"
+    puts "#{season.to_s.capitalize}:"
     holidays.each do |key , val|
-
+      
     end
 
   end
