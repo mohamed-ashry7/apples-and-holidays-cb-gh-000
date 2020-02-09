d8999b6dfd1f8fd6daa87e852f8c0559ea393458
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-09 15:05:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,5 +73,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  arr =[]
+  holiday_hash.each do |season , holidays|
+    holidays.each do |holiday , supplies|
+      arr << holiday if supplies.include?("BBQ")
+    end
+  end
 end
